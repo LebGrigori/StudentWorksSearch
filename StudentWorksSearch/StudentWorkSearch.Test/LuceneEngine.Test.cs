f8b9952edf80addf695cfa23651a5f7a3af21dad
erkatya <erkatya90@gmail.com>
2016-03-20 20:14:50 +0300

    Что-то похожее на тестовый метода для BuildIndex

--- StudentWorksSearch/StudentWorkSearch.Test/LuceneEngine.Test.cs
+++ StudentWorksSearch/StudentWorkSearch.Test/LuceneEngine.Test.cs
@@ -7,9 +7,91 @@ namespace StudentWorkSearch.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void BuildIndex_nullInObjectFieldSent_NullReferenceException()
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BuildIndex_nullAuthor_NullReferenceException()
         {
-            
+            StudentWorksSearch.LuceneSearch.LuceneEngine.BuildIndex(
+                new StudentWorksSearch.LuceneSearch.FileToIndex
+                {
+                    Id = 0,
+                    Authors= null,
+                    Description="test",
+                    Hashtags="test",
+                    Text="test",
+                    Title="test"           
+                });           
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BuildIndex_nullDescription_NullReferenceException()
+        {
+            StudentWorksSearch.LuceneSearch.LuceneEngine.BuildIndex(
+                new StudentWorksSearch.LuceneSearch.FileToIndex
+                {
+                    Id = 0,
+                    Authors = "test",
+                    Description = null,
+                    Hashtags = "test",
+                    Text = "test",
+                    Title = "test"
+                });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BuildIndex_nullHashtags_NullReferenceException()
+        {
+            StudentWorksSearch.LuceneSearch.LuceneEngine.BuildIndex(
+                new StudentWorksSearch.LuceneSearch.FileToIndex
+                {
+                    Id = 0,
+                    Authors = "test",
+                    Description = "test",
+                    Hashtags = null,
+                    Text = "test",
+                    Title = "test"
+                });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BuildIndex_nullText_NullReferenceException()
+        {
+            StudentWorksSearch.LuceneSearch.LuceneEngine.BuildIndex(
+                new StudentWorksSearch.LuceneSearch.FileToIndex
+                {
+                    Id = 0,
+                    Authors = "test",
+                    Description = "test",
+                    Hashtags = "test",
+                    Text = null,
+                    Title = "test"
+                });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BuildIndex_nullTitle_NullReferenceException()
+        {
+            StudentWorksSearch.LuceneSearch.LuceneEngine.BuildIndex(
+                new StudentWorksSearch.LuceneSearch.FileToIndex
+                {
+                    Id = 0,
+                    Authors = "test",
+                    Description = "test",
+                    Hashtags = "test",
+                    Text = "test",
+                    Title = null
+                });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BuildIndex_nullObject_NullReferenceException()
+        {
+            StudentWorksSearch.LuceneSearch.LuceneEngine.BuildIndex(
+                new StudentWorksSearch.LuceneSearch.FileToIndex());
         }
     }
 }
